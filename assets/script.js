50ca7bb8f11f976d122815b26cae650470beb7dc
jeremsers <jeremy.sersoub@hotmail.fr>
2023-05-30 21:42:07 +0200

    created function.js

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,27 @@
+import { chargerImage, changerPoints, selectImageRight, selectImageLeft, changerAnimImageRight, changerAnimImageLeft, animateProduct, animateQuestion, animateWhyus, animateContact } from "./function.js";
+
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
-
-let imageSelector = 0;
-let animimageSelector = 0;
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
 
 chargerImage();
 changerPoints();
 
 arrow_right.addEventListener("click", function () {
 	
-	animimageSelector = imageSelector
-	imageSelector===slides.length-1 ? imageSelector = 0 : imageSelector ++;
 	
-
+	
+	selectImageRight();
 	chargerImage();
 	changerAnimImageRight();
 	changerPoints();
 	
 });
+
 arrow_left.addEventListener("click", function () {
 
-	animimageSelector = imageSelector
-	imageSelector === 0 ? imageSelector = slides.length-1 : imageSelector --;
 	
-
+	
+	selectImageLeft();
 	chargerImage();
 	changerAnimImageLeft();
 	changerPoints();
@@ -49,93 +29,12 @@ arrow_left.addEventListener("click", function () {
 });
 
 
-function chargerImage() {
-	const parentElement = document.querySelector('.banner-div');
-	parentElement.innerHTML = ""
-	const currentImage = document.createElement("img");
-	currentImage.className = "banner-img"
-	const tagLine = document.createElement('p')
-	currentImage.src = `./assets/images/slideshow/${slides[imageSelector].image}`;
-	tagLine.innerHTML = slides[imageSelector].tagLine;
-	parentElement.appendChild(currentImage)
-	parentElement.appendChild(tagLine)
-	
-	
-};
-
-
-function changerAnimImageRight() {
-	const parentElementAnim = document.querySelector('.animation-div')
-	parentElementAnim.className = "animation-div reset-animimgR"
-	const parentElement = document.querySelector('.banner-div');
-	parentElement.className ="reset-currentimgR banner-div"
-	void parentElementAnim.offsetWidth;
-	parentElementAnim.innerHTML = ""
-	const animImage = document.createElement("img");
-	animImage.className = "banner-img"
-	const animtagLine = document.createElement('p')
-	animImage.src = `./assets/images/slideshow/${slides[animimageSelector].image}`;
-	animtagLine.innerHTML = slides[animimageSelector].tagLine;
-	parentElementAnim.appendChild(animImage)
-	parentElementAnim.appendChild(animtagLine)
-	parentElementAnim.className = "animation-animimgR animation-div"
-	parentElement.className ="animation-currentimgR banner-div"
-	
-};
-
-function changerAnimImageLeft() {
-	const parentElementAnim = document.querySelector('.animation-div')
-	parentElementAnim.className = "animation-div reset-animimgL"
-	const parentElement = document.querySelector('.banner-div');
-	parentElement.className ="reset-currentimgL banner-div"
-	void parentElementAnim.offsetWidth;
-	parentElementAnim.innerHTML = ""
-	const animImage = document.createElement("img");
-	animImage.className = "banner-img"
-	const animtagLine = document.createElement('p')
-	animImage.src = `./assets/images/slideshow/${slides[animimageSelector].image}`;
-	animtagLine.innerHTML = slides[animimageSelector].tagLine;
-	parentElementAnim.appendChild(animImage)
-	parentElementAnim.appendChild(animtagLine)
-	parentElementAnim.className = "animation-animimgL animation-div"
-	parentElement.className ="animation-currentimgL banner-div"
-	
-};
-
-
-
-function changerPoints() {
-	const parentElement = document.querySelector(".dots");
-	parentElement.innerHTML = "";
-	for (let i = 0; i < slides.length; i++) {
-		if (imageSelector === i){
-			const sliderElement = document.createElement("span");
-			sliderElement.className = "dot dot_selected";
-			parentElement.appendChild(sliderElement);
-			
-		}else{
-			const sliderElement = document.createElement("span");
-			sliderElement.className = "dot";
-			parentElement.appendChild(sliderElement);
-			
-		}
-	}
-
-};
-
-
-
-/*************Animation Products */
-
-
-
-
 window.addEventListener("scroll", function(){
 	animateProduct();
 	animateQuestion();
 	animateWhyus();
 	animateContact();
-	/* animateFooter(); */
+	
 
 
 })
@@ -143,73 +42,3 @@ window.addEventListener("scroll", function(){
 
 
 
-function animateProduct() {
-	const productImg = document.querySelector('.products-img');
-	const productTitle = document.querySelector('.products-title')
-	const productText = document.querySelector('.products-text');
-	const ElementRect = productImg.getBoundingClientRect(); 
-	if (ElementRect.top <= 817) {
-
-		productImg.classList.add('products-img-anim')
-		productTitle.classList.add('products-title-anim')
-		productText.classList.add('products-text-anim')
-	}
-}
-
-function animateQuestion() {
-	const ElementTitle = document.querySelector('.question-title');
-	const ElementText = document.querySelector('.question-text')
-	const ElementBtn = document.querySelector('.question-btn');
-	const ElementRect = ElementTitle.getBoundingClientRect(); 
-	if (ElementRect.top <= 884) {
-
-		ElementTitle.classList.add('question-title-anim')
-		ElementText.classList.add('question-text-anim')
-		ElementBtn.classList.add('question-btn-anim')
-	}
-}
-
-function animateWhyus() {
-	const ElementTitle = document.querySelector('.whyus-title');
-	const ElementArticle = document.querySelectorAll('.whyus-article')
-	const ElementRect = ElementTitle.getBoundingClientRect(); 
-	if (ElementRect.top <= 800) {
-
-		ElementTitle.classList.add('whyus-title-anim')
-		ElementArticle.forEach(element => {
-			element.classList.add('whyus-article-anim')
-		});
-		
-	}
-}
-
-function animateContact() {
-	const ElementTitle = document.querySelector('.contact-title');
-	const ElementDiv = document.querySelectorAll('.contact-div')
-	const ElementImg = document.querySelector('.contact-img')
-
-	const ElementRect = ElementTitle.getBoundingClientRect(); 
-	
-	if (ElementRect.top <= 900) {
-
-		ElementTitle.classList.add('contact-title-anim')
-		ElementDiv.forEach(element => {
-			element.classList.add('contact-div-anim')
-			
-		});
-		ElementImg.classList.add('contact-img-anim')
-
-		
-	}
-}
-
-/* function animateFooter() {
-	const ElementFooter = document.querySelector('footer');
-	const ElementRect = ElementFooter.getBoundingClientRect(); 
-	console.log(ElementRect.top)
-	if (ElementRect.top <= 1070) {
-
-		ElementFooter.classList.add('footer-anim')
-		
-	}
-} */
